b19cf22e18fb0e9fb0be94cbe5cb2cf7965f3fc7
Akaki Khimshiashvili <kakikhimshiashvili@gmail.com>
2025-04-02 14:33:43 +0400

    Images Gallery using tailwind and CRA

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.js",
+    "./src/components/ImageSearch.js",
+    "./src/components/ImageCard.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
